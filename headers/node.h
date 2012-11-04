4b5ec8901797e5ae8abe144034accc99fa89772d
Amadeusz Sadowski <amis.tau.92@gmail.com>
2012-11-04 21:40:47 +0100

    template classes implemented
    Pozwalam Ci się tym przerazić, schować się pod biurko, odczekać trzy
    minuty i dopiero wrócić przed ekran. to jest naprawdę brzydkie,
    nieestetyczne i trudne do przetrawienia. Ideologicznie najtrudniejsze
    jest usuwanie, więc na razie możesz tego nie oglądać, ale całą resztę
    proszę, proszę przejrzyj. Gdzie będą wątpliwości, dopisz w linijce
    komenta z pytaniem albo cokolwiek, a jak skończysz zrób commita, synca i
    puść smsa, żebym wiedział. Ja w wolnej chwili obejrzę i poprawię resztę
    plików, w szczególności deklaracje konstruktorów i przeładowań
    operatorów. Aha, zacznij od node, żeby się rozpędzić, jest prosty
    (wychodzę z założenia, że T potrafi się wypisać na strumień, nasza w tym
    rzecz żeby umiał).

--- headers/node.h
+++ headers/node.h
@@ -1,11 +1,10 @@
-#define NODE_h
-#ifndef NODE_h
 
 template <class T> // deklaracja ¿e to klasa-wzornik, z parametrem T, który musi byæ klas¹
+
 class Node { 
 
 public:
-    Node (T* value) {
+    Node<T> (T* value) {
         this->value = value; // inicjalizujê wskaŸnik wartoœci
         left = right = NULL; // podczas tworzenia nowego wêz³a jest on liœciem - nie ma synów
     };
@@ -13,7 +12,16 @@ public:
     T* value; // wskaŸnik na wartoœæ wêz³a
     
     Node<T> * left, * right; // wskaŸniki do lewego i prawego syna
+    
+    friend ostream& operator<< (ostream& out, const Node<T>& node)
+    {
+        if (left != NULL) {
+            out << *left;
+        }
+        out << *value << endl;
+        if (right != NULL) {
+            out << *right;
+        }
+    };
 };
 
-
-#endif
--- headers/tree.h
+++ headers/tree.h
@@ -1,21 +1,274 @@
-#define TREE_h
-#ifndef TREE_h
-
+/*
+ * class T has to implement single method:
+ * std::string& getTitle();
+ *
+ * the tree operates using titles of held objects
+ */
+ 
 template <class T>
-
-class Tree { // drzewo
-
+class Tree
+{
 public:
-    int count(); // to chyba miala byc ilosc elementow, nie?
-    bool add(T object); // dodaje objekt do drzewa
-    bool remove(T object); // usuwa objekt z drzewa
-    T* find(string Title); // wyszukiwanie wêz³a po nazwie
-    int compareTo(string); // ka¿de T musi to mieæ…
+    Tree();
+    
+    Tree(const Tree<T>& other);
+    
+    Tree<T>& operator=(const Tree<T>& other);
+    
+    friend ostream& operator<<(ostream& out, const Tree<T>& tree);
 
+    int count();
+    
+    /* searches for T* pointer such that
+    pointer == object,
+    and if such pointer is found returns false,
+    otherwise adds object to the tree and returns true */
+    bool add(T* object);
+    
+    /* searches for T* pointer such that
+    pointer->getTitle().compare(title) == 0,
+    and if such pointer is found removes it (only pointer, not object)
+    and returns true, otherwise returns false */
+    bool remove(const std::string& title);
+    
+    /* deletes nodes, but doesn't touch pointers held by them */
+    void clear();
+    
+    /* deletes objects pointed by nodes and all nodes themselves */
+    void seekAndDestroy();
+    
+    /* searches for T* pointer such that
+    object->getTitle().compare(title) == 0, and if such pointer is found,
+    returns it, otherwise returns NULL */
+    T* find(const std::string& title);
+           
 private:
-    Node<T> root; // korzeñ
-    T* findT(Node <T> x); // funkcja pomocnicza wyszukiwania
+    Node<T>* root;
+    
+    int count(Node<T>* node);
+    
+    bool addT(Node<T>* node, T* object);
+    
+    bool remove(Node<T>* node);
+    
+    void clear(Node<T>* node);
+    
+    void seekAndDestroy(Node<T>* node);
+    
+    Node<T>* findNode(Node<T>* node, const std::string& title);
 };
 
 
-#endif
+
+// definitions
+
+template <class T>
+Tree<T>::Tree()
+{
+    root = NULL;
+}
+
+template <class T>
+Tree<T>::Tree(const Tree<T>& other)
+{
+    root = other.root;
+}
+
+template <class T>
+Tree<T>& Tree<T>::operator=(const Tree<T>& other)
+{
+    if (root != other.root) {
+        clear();
+        root = other.root;
+    }
+    return *this;
+}
+
+template <class T>
+friend ostream& Tree<T>::operator<<(ostream& out, const Tree<T>& tree);
+{
+    if (root != NULL) {
+        out << *root;
+    }
+    return out;
+}
+
+/* counts number of nodes in tree */
+template <class T>
+int Tree<T>::count()
+{
+    return count(root);
+}
+
+/* searches for T* pointer such that
+pointer == object,
+and if such pointer is found returns false,
+otherwise adds object to the tree and returns true */
+template <class T>
+bool Tree<T>::add(T* object)
+{
+    if (object == NULL) {return false;}
+    if (root == NULL) {
+        root = new Node<T>(object);
+        return true;
+    }
+    return add(root, object);
+}
+
+/* searches for T* pointer such that
+pointer->getTitle().compare(title) == 0,
+and if such pointer is found removes it (only pointer, not object)
+and returns true, otherwise returns false */
+template <class T>
+bool Tree<T>::remove(const std::string& title)
+{
+    Node<T>* node = findNode(root, title);
+    if (node == NULL) {
+        return false;
+    }
+    root = remove(node, root);
+    return true;
+}
+
+/* deletes nodes, but doesn't touch pointers held by them */
+template <class T>
+void Tree<T>::clear()
+{
+    if (root != NULL) {
+        clear(root);
+        oot = NULL;
+    }
+}
+
+/* deletes objects pointed by nodes and all nodes themselves */
+template <class T>
+void Tree<T>::seekAndDestroy()
+{
+    if (root != NULL) {
+        seekAndDestroy(root);
+        root = NULL;
+    }
+}
+
+/* searches for T* pointer such that
+object->getTitle().compare(title) == 0, and if such pointer is found,
+returns it, otherwise returns NULL */
+template <class T>
+T* Tree<T>::find(const std::string& title)
+{
+    Node<T>* node = findNode(root, title);
+    if (node == NULL) {
+        return NULL;
+    }
+    return node->value;
+}
+
+template <class T>
+int Tree<T>::count(Node<T>* node)
+{
+    if (node == NULL) { return 0;}
+    return 1 + count(node->left) + count(node->right);
+}
+
+template <class T>
+bool Tree<T>::addT(Node<T>* node, T* object)
+{
+    int comparisonResult = object->getTitle().compare(node->value->getTitle());
+    if (comparisonResult < 0) {
+        //object is lesser than the one in node
+        if (node->left != NULL) {
+            return addT(node->left, object); // adding to left subtree
+        } else {
+            node->left = new Node<T>(object); // creating left subtree
+            return true; // object added
+        }
+    } else if (comparisonResult > 0) {
+        //object is greater than the one in node
+        if (node->right != NULL) {
+            return addT(node->right, object); // adding to right subtree
+        } else {
+            node->right = new Node<T>(object); // creating right subtree
+            return true; // object added
+        }
+    }
+    // object already in tree
+    return false;
+}
+
+/* removes 'wanted' from 'current' tree by checking if current is wanted,
+ * if not: calls remove on both children and assigns results to them
+ * if yes, checks:
+ *  - if node has no children, deletes and returns NULL
+ *  - if node has one child, deletes and returns child
+ *  - if node has two childs, checks which has greater size;
+ *      then assigns to current the value of greater child and calls to remove it
+ */
+template <class T>
+Node<T>* Tree<T>::remove(Node<T>* wanted, Node<T>* current)
+{
+    if (current != wanted) {
+        current->left = remove(current->left);
+        current->right = remove(current->right);
+    } else {
+        if (current->left == NULL && node->right == NULL) {
+            // node is leaf, we just delete it
+            delete wanted;
+            current = NULL;
+        } else if (current->left == NULL || node->right == NULL) {
+            // node has one child
+            current = current->left == NULL ? current->right : current->left;
+            delete wanted;
+            return current;
+        } else {
+            // node has two childs, check size of both
+            int leftSize = count(current->left);
+            int rightSize = count(current->right);
+            if (leftSize < rightSize) {
+                current->value = current->right->value;
+                current->right = remove(current->right, current->right);
+            } else {
+                current->value = current->left->value;
+                current->left = remove(current->left, current->left);
+            }
+        }
+    }
+    return current;
+}
+
+template <class T>
+void Tree<T>::clear(Node<T>* node)
+{
+    if (node->left != NULL) {clear(node->left);}
+    if (node->right != NULL) {clear(node->right);}
+    delete node;
+}
+
+template <class T>
+void Tree<T>::seekAndDestroy(Node<T>* node)
+{
+    if (node->left != NULL) {seekAndDestroy(node->left);}
+    if (node->right != NULL) {seekAndDestroy(node->right);}
+    delete node->value;
+    delete node;
+}
+
+template <class T>
+Node<T>* Tree<T>::findNode(Node<T>* node, const std::string& title)
+{
+    if (node == NULL) {
+        return NULL;
+    }
+    int comparisonResult = title.compare(node->value->getTitle());
+    if (comparisonResult == 0) {
+        //object found
+        return node;
+    }
+    if (comparisonResult < 0) {
+        // object lesser than the one in node, searching left subtree
+        return findNode(node->left, title);
+    } else {
+        // object greater than the one in node, searching right subtree
+        return findNode(node->right, title);
+    }
+}
+
